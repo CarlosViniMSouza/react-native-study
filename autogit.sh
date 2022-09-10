1766af8a40494447cdc760ec2ad2a795f25e9a16
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2022-09-10 17:27:45 -0300

    React Native Study -> started

---
+++ autogit.sh
@@ -0,0 +1,6 @@
+git add .
+git commit -m "React Native Study -> started"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/react-native-study.git
+git push -u origin main
+git status
